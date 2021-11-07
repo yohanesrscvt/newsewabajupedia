5a8278758a637089b5bbed87e3c967ffe78a38f5
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-11-07 21:45:02 +0700

    create header, footer, and welcome page

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <title>Document</title> --}}
+    <link rel="stylesheet" href="{{ asset('css/footer.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <!-- footer -->
+    <footer class="footer">
+        <div class="follow-link">
+            <p>Follow SewaBajuPedia</p>
+            <div class="social-media">
+                <a href="#" class="fa fa-facebook fa-2x"></a>
+                <a href="#" class="fa fa-twitter fa-2x"></a>
+                <a href="#" class="fa fa-instagram fa-2x"></a>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© Copyright 2021 PT. Sewa Baju Pedia</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ resources/views/master/header.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <title>Document</title> --}}
+    <link rel="stylesheet" href="{{ asset('css/header.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <!-- header -->
+    <header class="header">
+        <h1 class="header-title">SewaBajuPedia</h1>
+        <p class="header-par">Situs penyewa baju no.1 di Indonesia</p>
+    </header>
+</body>
+</html>

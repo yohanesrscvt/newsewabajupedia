41171301ae2675eb432a5265c1f8b1b105a49393
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-11-07 22:06:37 +0700

    create login and register page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet'>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body>
+    @include('master.header')
+    <div class="content">
+        <div class="form-list">
+            <div class="judul-login">
+                <h1>LOGIN</h1>
+            </div>
+            <!-- modified form -->
+            <form action="" method="post" class="form">
+                @csrf
+                <div class="email">
+                    <label for="email">Email</label>
+                    <br>
+                    <input type="email" name="email" id="email" required>
+                </div>
+
+                <div class="password">
+                    <label for="password">Password</label>
+                    <br>
+                    <input type="password" name="password" id="password" required>
+                </div>
+                
+                <button id="submit-button" type="submit" >Login</button>
+            </form>
+
+            <!-- alert message -->
+            @if(Session::get('fail'))
+                <script>
+                    swal("Sorry", "{{Session::get('fail')}}", "error");
+                </script>
+            @elseif(Session::get('success'))
+                <script>
+                    swal("Horay", "{{Session::get('success')}}", "success");
+                </script>
+            @endif
+            
+            <div class="already-login">
+                <p>Belum memiliki akun?</p>
+                <a href="">Daftar Sekarang</a>
+            </div>
+        </div>
+    </div>
+    @include('master.footer')
+</body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="{{ asset('css/register.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet'>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body>
+    @include('master.header')
+    <div class="content">
+        <div class="form-list">
+            <div class="judul-register">
+                <h1>REGISTER</h1>
+            </div>
+            
+            <!-- modified form -->
+            <form action="" method="post" class="form"> 
+                @csrf
+                <div class="name">
+                    <label for="nama">Nama Lengkap</label>
+                    <br>
+                    <input type="text" name="nama" id="nama" required>
+                </div>
+
+                <div class="email">
+                    <label for="email">Email</label>
+                    <br>
+                    <input type="email" name="email" id="email" required>
+                </div>
+
+                <div class="password">
+                    <label for="password">Password</label>
+                    <br>
+                    <input type="password" name="password" id="password" required>
+                </div>
+
+                <div class="conf-password">
+                    <label for="confirm_password">Konfirmasi Password</label>
+                    <br>
+                    <input type="password" name="confirm_password" id="confirm_password" required>
+                </div>
+                <button id="submit-button" type="submit" >Register</button>
+            </form>
+
+            <!-- alert message -->
+            @if(Session::get('fail'))
+                <script>
+                    swal("Sorry", "{{Session::get('fail')}}", "error");
+                </script>
+            @endif
+            <div class="already-register">
+                <p>Sudah memiliki akun?</p>
+                <a href="/login">Masuk Sekarang</a>
+            </div>
+        </div>    
+    </div>
+    @include('master.footer')
+</body>
+</html>
